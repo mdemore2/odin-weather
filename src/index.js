5264aedf8afb6acda4cc88d7a9613c1cb25dfb94
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2023-11-25 14:38:04 -0500

    Added api functions to index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ function createPage() {
   div.classList.add("container");
   var cityInput = document.createElement("input");
   cityInput.type = "text";
+  cityInput.id = "city";
+  cityInput.minLength = 2;
   var cityLabel = document.createElement("label");
   cityLabel.textContent = "City:\t";
   var submitBtn = document.createElement("button");
@@ -21,7 +23,33 @@ function createPage() {
   body.appendChild(div);
 
   var weatherDiv = document.createElement("div");
+  weatherDiv.classList.add("weather");
   body.appendChild(weatherDiv);
 }
 
-function getWeather(e) {}
+async function getWeather(e) {
+  var city = document.getElementById("city").value;
+  const coords = await getCoords(city);
+  const lat = coords[0];
+  const lon = coords[1];
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,precipitation,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m`
+  );
+  const responseObj = await response.json();
+  console.log(responseObj);
+}
+
+async function getCoords(city) {
+  const response = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+  );
+  const responseObj = await response.json();
+  const results = responseObj.results;
+  console.log(results);
+  const lat = results[0].latitude;
+  const lon = results[0].longitude;
+  console.log(lat);
+  console.log(lon);
+  var coords = [lat, lon];
+  return coords;
+}
